7bc4af5aeb75c3fc4d20cd42d2193fe4c77c580d
NiclasMuss <niclas.muss@yahoo.de>
2019-01-07 21:01:03 +0100

    Coding Guidelines in Android Launcher

--- android/src/com/muss_and_toeberg/snake_that/AndroidLauncher.java
+++ android/src/com/muss_and_toeberg/snake_that/AndroidLauncher.java
@@ -14,15 +14,19 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		//check for Storage Permission
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-			if (this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-				this.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-				//TODO: denying Access on first launch Crashes the App
+			if (this.checkSelfPermission(
+			        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+				this.requestPermissions(new String[]{
+				        Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                //TODO: denying Access on first launch Crashes the App
 			}
 		}
 
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		AndroidApplicationConfiguration config =
+                new AndroidApplicationConfiguration();
 		config.useAccelerometer = false;
 		config.useCompass = false;
 		initialize(new MainGame(), config);
